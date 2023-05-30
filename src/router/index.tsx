3c462465faf995451f12593c71f0315650e4b1f0
holdenn1 <caulfieldd17@gmail.com>
2023-05-30 18:44:53 +0300

    published on git pages

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createHashRouter } from 'react-router-dom';
 import App from '../App';
 import HomePage from '../pages/HomePage/HomePage';
 import ErrorPage from '../pages/ErrorPage/ErrorPage';
@@ -12,56 +12,56 @@ import LibraryPage from '../pages/LibraryPage/LibraryPage';
 import MessagesPage from '../pages/AccountPages/MessagesPage';
 import ChatWindow from '../components/Library/Chat/ChatWindow/ChatWindow';
 
-export const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <App />,
-		errorElement: <ErrorPage />,
-		children: [
-			{
-				index: true,
-				element: <HomePage />,
-			},
-			{
-				path: 'sign-up',
-				element: <SignUpPage />,
-			},
-			{
-				path: 'sign-in',
-				element: <SignInPage />,
-			},
-			{
-				path: 'library',
-				element: <LibraryPage />,
-			},
-			{
-				path: 'book-shelf',
-				element: <BookshelfPage />,
-				children: [
-					{
-						path: 'new-book',
-						element: <NewBookPage />,
-					},
-					{
-						path: 'all-books',
-						element: <AllBookPage />,
-					},
-					{
-						path: 'favorite-books',
-						element: <FavoriteBooksPage />,
-					},
-					{
-						path: 'messages',
-						element: <MessagesPage />,
-						children: [
-							{
-								path: `:chatId`,
-								element: <ChatWindow />,
-							},
-						],
-					},
-				],
-			},
-		],
-	},
+export const router = createHashRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: 'sign-up',
+        element: <SignUpPage />,
+      },
+      {
+        path: 'sign-in',
+        element: <SignInPage />,
+      },
+      {
+        path: 'library',
+        element: <LibraryPage />,
+      },
+      {
+        path: 'book-shelf',
+        element: <BookshelfPage />,
+        children: [
+          {
+            path: 'new-book',
+            element: <NewBookPage />,
+          },
+          {
+            path: 'all-books',
+            element: <AllBookPage />,
+          },
+          {
+            path: 'favorite-books',
+            element: <FavoriteBooksPage />,
+          },
+          {
+            path: 'messages',
+            element: <MessagesPage />,
+            children: [
+              {
+                path: `:chatId`,
+                element: <ChatWindow />,
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
 ]);
